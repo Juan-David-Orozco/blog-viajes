cf0ae4ff2cffbd52fabf947028f0053b039d1580
Juan-David-Orozco <ing.ele2018@gmail.com>
2021-12-13 17:28:17 -0500

    modulo pagina autores

--- routes/publics.js
+++ routes/publics.js
@@ -177,4 +177,42 @@ router.get('/publicacion/:id', (peticion, respuesta) => {
 })
 // -------      Fin Ruta detalle publicación -------------- //
 
+// -------      Modulo Autores -------------- //
+router.get('/autores', (peticion, respuesta) => {
+
+  pool.getConnection((err, connection) => {
+    const consulta = `
+      SELECT autores.id id, pseudonimo, avatar, publicaciones.id publicacion_id, titulo
+      FROM autores
+      INNER JOIN
+      publicaciones ON
+      autores.id = publicaciones.autor_id
+      ORDER BY autores.id DESC, publicaciones.fecha_hora DESC
+    `
+    connection.query(consulta, (error, filas, campos) => {
+      autores = []
+      ultimoAutorId = undefined
+      filas.forEach(registro => {
+        if (registro.id != ultimoAutorId){
+          ultimoAutorId = registro.id
+          autores.push({
+            id: registro.id,
+            pseudonimo: registro.pseudonimo,
+            avatar: registro.avatar,
+            publicaciones: []
+          })
+        }
+        autores[autores.length-1].publicaciones.push({
+          id: registro.publicacion_id,
+          titulo: registro.titulo
+        })
+      });
+      respuesta.render('autores', { autores: autores })
+    })
+    connection.release()
+  })
+
+})
+// -------     Fin Modulo Autores -------------- //
+
 module.exports = router
